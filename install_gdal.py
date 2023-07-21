84fa3b7bdc146e4763d15eb086315af2f6ae108b
lefcgis <lefcgis@gmail.com>
2023-07-21 07:27:50 -0500

    modif_2

---
+++ install_gdal.py
@@ -0,0 +1,6 @@
+conda install -c conda-forge gdal
+conda install -c "conda-forge/label/TEST" gdal
+conda install -c "conda-forge/label/broken" gdal
+conda install -c "conda-forge/label/cf201901" gdal
+conda install -c "conda-forge/label/cf202003" gdal
+conda install -c "conda-forge/label/gcc7" gdal
